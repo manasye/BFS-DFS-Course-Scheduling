498480b6ac6a4ff9f679c65d681600c0f8b9cc89
manasyebukit <manasyebukit@gmail.com>
2018-03-10 20:40:08 +0700

    add split text functionality

--- Course_Scheduling/Course_Scheduling/Program.cs
+++ Course_Scheduling/Course_Scheduling/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Course_Scheduling
@@ -11,20 +12,50 @@ namespace Course_Scheduling
     {
         static void Main(string[] args)
         {
-            //string[] subject = "blabla";
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\manasyebukit\Documents\GitHub\BFS-DFS-Courses-Scheduling\file.txt");
-            System.Console.WriteLine("Contents of file.txt = ");
-            foreach (string line in lines)
+            string content = File.ReadAllText(@"C:\Users\manasyebukit\Documents\GitHub\BFS-DFS-Courses-Scheduling\file.txt");
+            var paragraphs = content.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string[][] subjects = new string[paragraphs.Length][];
+
+            for (int i = 0; i < subjects.Length; i++)
             {
-                //subject = line.Split('.');
-                Console.WriteLine(line);
+                subjects[i] = paragraphs[i].Split(new char[] { ',' , ' ' , '\n' }, StringSplitOptions.RemoveEmptyEntries);
             }
-            //Console.WriteLine(subject);
-            //foreach (var x in subject)
+
+            for (int i = 0; i < subjects.Length; i++)
+            {   
+                for (int j = 0; j < subjects[i].Length; j++)
+                {
+                    Console.WriteLine(subjects[i][j]);
+                }
+            }
+            //var numOfLines = lines.Length;
+            //Console.WriteLine(numOfLines);
+
+            //for (int i = 0;i < numOfLines; i++)
             //{
-            //    System.Console.WriteLine($"<{x}>");
+            //    Console.WriteLine(lines[i]);
             //}
 
-        }   
+            //for (int i = 0; i < numOfLines; i++)
+            //{
+            //    //subject = lines[i].Split(new[] { ',', ' '}, StringSplitOptions.RemoveEmptyEntries);
+            //    //for (int j = 0; j < subject.Length; j++)
+            //    //{
+            //    //    Console.WriteLine(subject[j][0]);
+            //    //}
+            //    subject = lines[i].Split(',').Select(
+            //    x => new string[2] { x.Substring(0, 2), x.Substring(2, 2) }
+            //    ).ToArray();
+            //}
+
+            //var subject = lines.Split(',')
+            //    .Select(p => Regex.Split(p, "(?<=\\G.{2})"))
+            //    .ToArray();
+
+            //Console.WriteLine(subject[2][0]);
+
+
+        }
     }
 }
